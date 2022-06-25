d1bfc5fddb5389661e35d1967fb0f2c90d954cf9
Pavithracsekar <pavithracsekar7@gmail.com>
2022-06-25 19:23:03 +0530

    2nd Commit

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
 'use strict';
-/* 
-//Start Guessing
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number';
-console.log(document.querySelector('.message').textContent);
 
-//?
-console.log(document.querySelector('.number').textContent);
-document.querySelector('.number').textContent = 13;
-console.log(document.querySelector('.number').textContent);
-
-//Score
-console.log(document.querySelector('.score').textContent);
-document.querySelector('.score').textContent = 10;
-console.log(document.querySelector('.score').textContent);
-
-//Input feild
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value); */
-
-//On click-do Event Handling
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-//After DRY principle- created function to replace common message code
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
 
+const displaySecretNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+const manipulateColor = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+
+const manipulateWidth = function (width) {
+  document.querySelector('body').style.backgroundColor = width;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+//Adding Event Handler for CLICK button and Actual Logic of Guess My Number Game
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  // console.log(guess, typeof guess);
 
   if (!guess) {
     //No input is entered
-    //document.querySelector('.message').textContent = '⛔ No Number!';
     displayMessage('⛔ No Number!');
   } else if (guess === secretNumber) {
     //Number is same as Secret no.
-    // document.querySelector('.message').textContent = '🏆Correct Number!';
     displayMessage('🏆Correct Number!');
-    document.querySelector('.number').textContent = secretNumber;
+    displaySecretNumber(secretNumber);
 
     //css style mainupaltion
-    document.querySelector('body').style.backgroundColor = ' #60b347';
-    document.querySelector('.number').style.width = '30rem';
+    manipulateColor('#60b347');
+    manipulateWidth('30rem');
 
     //setting HighScore
     if (score > highScore) {
@@ -54,38 +46,17 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highScore;
     }
   } else if (guess !== secretNumber) {
+    //Checks if number is higher or lower than SecretNumber
     if (score > 1) {
-      /* document.querySelector('.message').textContent =
-        guess > secretNumber ? '📈 Too High!' : '📉 Too Low!';
-      score--; */
       displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      // document.querySelector('.message').textContent = '☹ You Lost!';
+      //if Score recheas 0, display Lost
       displayMessage('☹ You Lost!');
+      displayScore(0);
     }
   }
-  //Before Applying DRY Principle
-  /* else if (guess > secretNumber) {
-    //Number is higher than Secret no.
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '☹ You Lost!';
-    }
-  } else if (guess < secretNumber) {
-    //Number is lower than Secret no.
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '☹ You Lost!';
-    }
-  } */
 });
 
 //Adding Event Handler for AGAIN button and Resetting GAME
@@ -93,12 +64,11 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  //document.querySelector('.message').textContent = 'Start guessing...';
   displayMessage('Start guessing...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
+  displayScore(score);
+  displaySecretNumber('?');
   document.querySelector('.guess').value = null;
 
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  manipulateColor('#222');
+  manipulateWidth('15rem');
 });
